ed17d57ec208569e68c8c07e6ef37a1116ce0cda
Khawaja Usman <94803671+usmanTheCoder@users.noreply.github.com>
2024-09-12 20:15:34 +0500

    Add components/icons/index.ts

---
+++ components/icons/index.ts
@@ -0,0 +1,9 @@
+import { FaChartBar, FaChartLine, FaChartPie, FaScatterPlot } from 'react-icons/fa';
+import { HiUser, HiLogout } from 'react-icons/hi';
+
+export const ChartIcon = FaChartBar;
+export const LineChartIcon = FaChartLine;
+export const PieChartIcon = FaChartPie;
+export const ScatterPlotIcon = FaScatterPlot;
+export const UserIcon = HiUser;
+export const LogoutIcon = HiLogout;
